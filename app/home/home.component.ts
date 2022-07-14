2a33c1be8479642db138a833fa152cf0c90be8f8
shubhangitayade2 <105914219+shubhangitayade2@users.noreply.github.com>
2022-07-14 09:19:16 +0530

    angular

---
+++ app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/Product';
+import { ProductService } from '../services/product.service';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  
+
+  constructor(private _productservice: ProductService) { }
+
+  products: Array<Product> = new Array<Product>();
+  ngOnInit(): void {
+
+    this._productservice.getProducts().subscribe(res => this.products = res, err => console.log(err))
+  }
+
+}
