3542158617f5bbc46cab28f4c4dacb103e20257a
shubhangitayade2 <105914219+shubhangitayade2@users.noreply.github.com>
2022-07-14 09:20:42 +0530

    visual studio

---
+++ Controllers/PaymentController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EcommerceWebApi.Models;
+namespace EcommerceWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        EcommerceDBContext db;
+
+        public PaymentController(EcommerceDBContext _db)
+        {
+            db = _db;
+        }
+        [HttpGet]
+        public IEnumerable<TblPayment> GetPayments()
+        {
+            return db.TblPayments;
+        }
+        [HttpPost]
+        public string Post([FromBody] TblPayment payment)
+        {
+            db.TblPayments.Add(payment);
+            db.SaveChanges();
+            return "success";
+        }
+
+    }
+    
+
+
+
+}
---
+++ Models/EcommerceDBContext.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace EcommerceWebApi.Models
+{
+    public partial class EcommerceDBContext : DbContext
+    {
+        public EcommerceDBContext()
+        {
+        }
+
+        public EcommerceDBContext(DbContextOptions<EcommerceDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblCategory> TblCategories { get; set; }
+        public virtual DbSet<TblLogin> TblLogins { get; set; }
+        public virtual DbSet<TblPayment> TblPayments { get; set; }
+        public virtual DbSet<TblProduct> TblProducts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-RA073J5;Initial Catalog=EcommerceDB;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<TblCategory>(entity =>
+            {
+                entity.ToTable("TblCategory");
+
+                entity.Property(e => e.CatName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TblLogin>(entity =>
+            {
+                entity.ToTable("TblLogin");
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TblPayment>(entity =>
+            {
+                entity.ToTable("TblPayment");
+
+                entity.Property(e => e.City)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.StreedAddress)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TblProduct>(entity =>
+            {
+                entity.ToTable("TblProduct");
+
+                entity.Property(e => e.ProductDescription)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ProductDiscount).HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.ProductFinal).HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.ProductImage)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ProductMrp).HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.ProductName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
